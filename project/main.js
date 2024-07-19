f8e30c05e209c08f39dee35ede2568f8dd834756
Sonwabiso <sonwabisolebokgang@gmail.com>
2024-07-19 12:00:56 +0200

    added header and subheader

--- project/main.js
+++ project/main.js
@@ -1,10 +1,14 @@
 import './style.css'
 
 var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=&' +
+          'country=za&' +
           'apiKey=2b939a9e1bc94029add973cc786c3d86';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
+
+fetch(url)
+    .then(response => response.json()) // Convert the response to JSON
+    .then(data => {
+        console.log(data); // Log the data
     })
+    .catch(error => {
+        console.error('Error fetching the news:', error); // Handle any errors
+    });
