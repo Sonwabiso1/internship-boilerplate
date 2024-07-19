d71fe48b3b3cac0bc7131086ba11acc0636d814e
Sonwabiso <sonwabisolebokgang@gmail.com>
2024-07-19 11:13:01 +0200

    added tailwind

--- project/main.js
+++ project/main.js
@@ -1,1 +1,10 @@
 import './style.css'
+
+var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=&' +
+          'apiKey=2b939a9e1bc94029add973cc786c3d86';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
---
+++ project/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './style.css' 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
